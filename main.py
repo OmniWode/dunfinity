694ad40e5e9ee72647428dc474097c96aed79707
Parker <omniwode@gmail.com>
2024-06-13 22:49:23 -0400

    barebones curses

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
+import curses
 
-def main():
-	print("hello world!")
+def main(stdscr):
+	# clear screen
+	stdscr.clear()
+
+	# retrieve screen height and width
+	height, width = stdscr.getmaxyx()
+
+	# display a message
+	message = "hello, curses!"
+	x = width // 2 - len(message) // 2
+	y = height // 2
+	stdscr.addstr(y, x, message)
+
+	# wait for user input
+	stdscr.refresh()
+	stdscr.getch()
 
 if __name__=="__main__":
-	main()
+	# start the curses application
+	curses.wrapper(main)
